8990f475064bef863b9ed4d9f642b6240e123178
George-Laptop <glthomas526@gmail.com>
2023-03-23 10:48:19 -0400

    added department view & inquirer navigation

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 require("dotenv").config(); //.env mySQL data protection
+
+let departmentTable = [];
 
 // Middleware
 app.use(express.json()); // Parse JSON requests
@@ -18,19 +22,109 @@ const connection = mysql.createConnection({
 
 connection.query(
   "SELECT * FROM `departments`",
-  function (err, results, fields) {
-    console.log(err);
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
+  function (err, results, fields) {}
 );
 
+initialPrompt();
+
+function initialPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "activity",
+        message: "Main Menu",
+        choices: ["See Departments", "See Roles", "See Employees"],
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return "You must enter at least one character.";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      if (answers.activity == "See Departments") {
+        departmentTable = [];
+        connection.query(
+          "SELECT * FROM `departments`",
+          function (err, results, fields) {
+            for (let i = 0; i < results.length; i++) {
+              departmentTable.push({
+                id: `${results[i].department_id}`,
+                name: `${results[i].department_name}`,
+              });
+            }
+            console.log(" ");
+            console.table(departmentTable);
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  name: "navmenu",
+                  message: "Departments",
+                  choices: ["Add New Department", "Go Back to Main Menu"],
+                },
+              ])
+              .then((answers) => {
+                if (answers.navmenu == "Go Back to Main Menu") {
+                  initialPrompt();
+                }
+                if (answers.navmenu == "Add New Department") {
+                  inquirer
+                    .prompt([
+                      {
+                        type: "input",
+                        name: "addDepartment",
+                        message:
+                          "What is the name of the department you want to create? (CTRL+C to Quit.)",
+                        validate: function (answer) {
+                          if (answer.length < 1) {
+                            return "You must enter at least one character.";
+                          }
+                          return true;
+                        },
+                      },
+                    ])
+                    .then((answers) => {
+                      console.log(answers.addDepartment);
+                      connection.query(
+                        `INSERT INTO departments (department_name) VALUES ("${answers.addDepartment}")`,
+                        function (err, results, fields) {
+                          if (err) throw err;
+                          console.log("Added successfully!");
+                          inquirer
+                            .prompt([
+                              {
+                                type: "list",
+                                name: "navmenu",
+                                message: "Departments",
+                                choices: [
+                                  "Add New Department",
+                                  "Go Back to Main Menu",
+                                ],
+                              },
+                            ])
+                            .then((answers) => {
+                              if (answers.navmenu == "Go Back to Main Menu") {
+                                initialPrompt();
+                              }
+                            });
+                        }
+                      );
+                    });
+                }
+              });
+          }
+        );
+      }
+    });
+}
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app.listen(port, () => {});
